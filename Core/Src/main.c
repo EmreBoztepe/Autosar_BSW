e71fe2ea4ed5c448fbd86bc1e6fd682474c4869e
EmreBoztepe <61236072+EmreBoztepe@users.noreply.github.com>
2023-02-12 22:10:06 +0300

    Debug configuration fixed.
    Now, the project can debug on STM32CubeIDE....
    My ide version is 1.11.2...
    Eclipse may use instead of CubeIDE

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -147,8 +147,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
-  //__HAL_RCC_GPIOH_CLK_ENABLE();
-  //__HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13|GPIO_PIN_14, GPIO_PIN_RESET);
